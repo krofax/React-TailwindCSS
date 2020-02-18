6c4b5ab4dcb01de0506940cf791dc1b625330f64
Blessing Krofegha <bkrofegha@gmail.com>
2020-02-18 10:55:27 +0100

    initial files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import React from 'react';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img className="w-full" src={require('./profile.jpg')} alt="Display" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Blessing Krofegha
+        </div>
+        <p className="text-gray-700 text-base">
+          When i'm not coding i switch to netflix with biscuits and cold tea as my companion. <span></span>😜
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div> 
+      <div className="px-6 py-4">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#Software Engineer</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#Writter</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mt-2 ml-20">#Public Speaker</span>
+      </div>
     </div>
   );
 }
-
 export default App;
